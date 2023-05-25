cd43d39a1d664af2fba0eab79c3dce744e164d01
Xicheng Tang <hellomico0520@hotmail.com>
2023-05-25 17:13:57 -0400

    Using postgres

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import psycopg2
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
