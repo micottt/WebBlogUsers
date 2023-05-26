99e5947bf8f0798c0470586cabf7c42fbc4bef64
Xicheng Tang <hellomico0520@hotmail.com>
2023-05-25 22:08:09 -0400

    set Img_URL placeholder

--- forms.py
+++ forms.py
@@ -5,12 +5,19 @@ from flask_ckeditor import CKEditorField
 
 
 ##WTForm
+
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()],
+                          render_kw={"placeholder": "Leaving this field blank sets a default image."})
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
+    #
+    # def __init__(self, *args, **kwargs):
+    #     super(CreatePostForm, self).__init__(*args, **kwargs)
+    #     self.img_url.default = "https://media.istockphoto.com/id/1182434606/photo/reflections-of-sunset-with-cloudscape-in-lake-water.jpg?s=612x612&w=0&k=20&c=82Y10VMTzKG2e6IZ1amcuQjgeEAhTeKqvzCpGgsytFo="
+    #     self.process()
 
 
 class RegisterForm(FlaskForm):
--- main.py
+++ main.py
@@ -18,14 +18,14 @@ my_email = "xichengtang520@gmail.com"
 password = "ofefwubgaldsbebv"
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
